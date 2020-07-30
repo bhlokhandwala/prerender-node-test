ecd59a6cfe3457e7c16ddace6d47862aece432ca
burhanMedlabz <burhanuddin.lokhandwala@medlabz.com>
2020-07-30 18:10:39 +0530

    Adding Caching

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const prerender = require('prerender');
+const prerender_memory_cache = require('prerender-memory-cache')
 const server = prerender();
+server.use(prerender_memory_cache);
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
